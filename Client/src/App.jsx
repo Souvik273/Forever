34e61ccb01b45fd513dccf85e2d757d4cd417866
Souvik Goswami <souvikgoswami464@gmail.com>
2024-12-20 18:20:07 +0000

    Routes created for different pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import PlaceOrders from './pages/PlaceOrders'
+import Orders from './pages/Orders'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      Hello Je welcome
-    </>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+            <Route path='/collection' element={<Collection/>}/>
+            <Route path='/product/:productId' element={<Product/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/place-order' element={<PlaceOrders/>}/>
+            <Route path='/orders' element={<Orders/>}/>
+        </Routes>
+    </div>
   )
 }
 
