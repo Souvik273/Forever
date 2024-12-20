78af6d99b5cddb57a5173426e4acc4ead9a820f3
Souvik273 <souvikgoswami464@gmail.com>
2024-12-21 00:52:44 +0530

    Navbar committed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,11 +7,14 @@ import Contact from './pages/Contact'
 import Login from './pages/Login'
 import PlaceOrders from './pages/PlaceOrders'
 import Orders from './pages/Orders'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
